e3c26a7772222b786be8379833581643d695d9ac
Alejandro Llorente <alejandro22cuba@hotmail.com>
2023-09-09 12:48:59 +0200

    The content in the data folder has been separated

---
+++ src/data/education.js
@@ -0,0 +1,19 @@
+import imgEdicacionFisica from '@assets/images/educacionFisica.jpg';
+import imgBachiller from '@assets/images/bachiller.jpg';
+
+export default [
+  {
+    title: 'Profesor Educación Física',
+    subtitle: 'Manuel Fajardo (EPEF), Habana, Cuba',
+    year: 2005,
+    image: imgEdicacionFisica,
+    alt: 'Imagen titulacion profesor de educación física',
+  },
+  {
+    title: 'Bachiller',
+    subtitle: 'Manuel Permui (ESPA), Habana, Cuba',
+    year: 2002,
+    image: imgBachiller,
+    alt: 'Imagen titulacion bachiller',
+  },
+];
