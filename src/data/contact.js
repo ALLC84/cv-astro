cd30440b2e80cc915a671c794e27d159e8284b19
Alejandro Llorente <alejandro22cuba@hotmail.com>
2023-09-12 16:25:02 +0200

    Several structure and design changes

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,19 +1,13 @@
 import imageAvatar from '@assets/images/imgabout.jpg';
-import CallIcon from '@assets/images/call_white.svg';
 import EmailIcon from '@assets/images/email_white.svg';
 import LinkIcon from '@assets/images/link_white.svg';
 
 export default {
-  name: 'Alejandro Llorente',
+  name: 'Alejandro LLorente',
   profession: 'Desarrollador Frontend',
   about:
     'Desarrollador web con experiencia utilizando Vue y React. Actualmente ocupo un puesto de desarrollador fontend en Conisel15,  Madrid, trabajando en proyectos para Ald Automotive.',
   image: imageAvatar,
-  call: {
-    phone: 618377176,
-    title: 'Lláme',
-    icon: CallIcon
-  },
   mail: {
     email: 'alejandro22cuba@hotmail.com',
     title: 'Envía un correo',
